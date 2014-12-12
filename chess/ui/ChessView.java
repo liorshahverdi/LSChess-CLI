6a44428de154de09d3b6a737d3681e27a7e2df64
unknown <liorshahverdi@gmail.com>
2014-12-12 18:38:05 -0500

    added base classes to model directory

---
+++ chess/ui/ChessView.java
@@ -0,0 +1,11 @@
+import javax.swing.JFrame;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class ChessView extends JFrame implements ActionListener{
+	private static final int WIDTH = 850;
+	private static final int HEIGHT = 850;
+
+	
+}
