f430ce975c80f737b827baaa94a75340e7aee86b
unknown <liorshahverdi@gmail.com>
2014-12-14 03:45:59 -0500

    .

--- chess/ui/TileArea.java
+++ chess/ui/TileArea.java
@@ -3,10 +3,24 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 
-public class TileArea extends JPanel implements ActionListener{
+public class TileArea implements MouseListener, MouseMotionListener{
 	
 
 	public TileArea(){
 
 	}
+
+	public void mouseClicked(MouseEvent e){}
+
+	public void mouseEntered(MouseEvent e){}
+
+	public void mouseExited(MouseEvent e){}
+
+	public void mousePressed(MouseEvent e){}
+
+	public void mouseReleased(MouseEvent e){}
+
+	public void mouseDragged(MouseEvent e){}
+
+	public void mouseMoved(MouseEvent e){}
 }
