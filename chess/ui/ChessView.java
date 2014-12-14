4b94d6f1dcb0faf8eaa94eb2a9094c6875589416
unknown <liorshahverdi@gmail.com>
2014-12-13 21:28:32 -0500

    blank red GUI

--- chess/ui/ChessView.java
+++ chess/ui/ChessView.java
@@ -7,5 +7,27 @@ public class ChessView extends JFrame implements ActionListener{
 	private static final int WIDTH = 850;
 	private static final int HEIGHT = 850;
 
-	
+	public ChessView(){
+		setTitle("LS Chess Game");
+		add(getJContentPane());
+		setSize(WIDTH,HEIGHT);
+		setVisible(true);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+	}
+
+	private JPanel getJContentPane(){
+		JPanel temp = new JPanel();
+		temp.setBackground(Color.red);
+		return temp;
+	}
+
+	public void actionPerformed(ActionEvent e){
+		
+	}
+
+	public static void main(String[] args) {
+		try {
+			ChessView cv = new ChessView(); 
+		}catch(Exception e){System.out.println(e.getMessage());} 
+	}
 }
