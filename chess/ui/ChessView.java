88194e1c7e85dbd7b1ffe72007434afbd87829e8
unknown <liorshahverdi@gmail.com>
2014-12-14 01:10:15 -0500

    havent found a nice way to do this yet

--- chess/ui/ChessView.java
+++ chess/ui/ChessView.java
@@ -3,9 +3,11 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 
-public class ChessView extends JFrame implements ActionListener{
+public class ChessView extends JFrame implements MouseListener, MouseMotionListener {
 	private static final int WIDTH = 850;
 	private static final int HEIGHT = 850;
+	private static JPanel gui_board;
+	private ImageIcon black_tile, white_tile;
 
 	public ChessView(){
 		setTitle("LS Chess Game");
@@ -18,13 +20,34 @@ public class ChessView extends JFrame implements ActionListener{
 	private JPanel getJContentPane(){
 		JPanel temp = new JPanel();
 		temp.setBackground(Color.red);
+		temp.setLayout(new FlowLayout());
+
+		black_tile = new ImageIcon("images\\Black-Marble.jpg");
+		white_tile = new ImageIcon("images\\White-Marble.jpg");
+
+		gui_board = new JPanel();
+		gui_board.setLayout(new GridLayout(8,8));
+		temp.add(gui_board);
+
+		JPanel tile1 = new JPanel();
+
 		return temp;
 	}
 
-	public void actionPerformed(ActionEvent e){
-		
-	}
+	public void mouseClicked(MouseEvent me) {}
 
+	public void mousePressed(MouseEvent me) {}
+
+	public void mouseReleased(MouseEvent me) {}
+
+	public void mouseEntered(MouseEvent me) {}
+
+	public void mouseExited(MouseEvent me) {}
+
+	public void mouseMoved(MouseEvent me){}
+
+	public void mouseDragged(MouseEvent me){}
+	
 	public static void main(String[] args) {
 		try {
 			ChessView cv = new ChessView(); 
