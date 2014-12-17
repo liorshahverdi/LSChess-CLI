946eb47137d5af7062fba5876320f0c1ba877eef
unknown <liorshahverdi@gmail.com>
2014-12-17 01:48:46 -0500

    loving every minute

--- chess/model/CG.java
+++ chess/model/CG.java
@@ -2,19 +2,24 @@ import java.util.Properties;
 import java.util.ArrayList;
 public class CG{
 	public static String[][] board;
+	public static int cp;//will be modded by 2 at each iteration
+	public ArrayList<ChessPiece> offBoard;
 
 	public CG(){
+		cp = 1;
 		board = new String[][]{
 			{"w_c","w_n","w_b","w_k","w_q","w_b","w_n","w_c"},
 			{"w_p","w_p","w_p","w_p","w_p","w_p","w_p","w_p"},
-			{"-","-","-","-","-","-","-","-"},
-			{"-","-","-","-","-","-","-","-"},
-			{"-","-","-","-","-","-","-","-"},
-			{"-","-","-","-","-","-","-","-"},
+			{"-",  "-"  ,"-"  ,"-"  ,"-"  ,"-"  ,"-"  ,"-"},
+			{"-",  "-"  ,"-"  ,"-"  ,"-"  ,"-"  ,"-"  ,"-"},
+			{"-",  "-"  ,"-"  ,"-"  ,"-"  ,"-"  ,"-"  ,"-"},
+			{"-",  "-"  ,"-"  ,"-"  ,"-"  ,"-"  ,"-"  ,"-"},
 			{"b_p","b_p","b_p","b_p","b_p","b_p","b_p","b_p"},
 			{"b_c","b_n","b_b","b_k","b_q","b_b","b_n","b_c"}
 		};
+		offBoard = new ArrayList<ChessPiece>();
 		//printMat(board);
+		System.out.println("Welcome to LSChess! Let's play!");
 		startGame();
 	}
 
@@ -32,14 +37,17 @@ public class CG{
 		private static ArrayList<Properties> possibleMoves(){
 			ArrayList<Properties> temp = new ArrayList<Properties>();
 
-			int num_empty_tiles = 0;
-
+			int num_whites = 0;
+			int num_blacks = 0;
 			for (String[] row : board){
 				for (String x : row){
 					ChessPiece thisPiece = ChessPiece.getEnum(x);
-					System.out.println("enum="+thisPiece);
+					if (isWhite(thisPiece)) num_whites++;
+					if (isBlack(thisPiece)) num_blacks++;
 				}
 			}
+
+			System.out.println("b="+num_blacks+"\tw="+num_whites);
 
 			return temp;
 		}
@@ -49,6 +57,22 @@ public class CG{
 				if (str.equals(t.getStr())) return t;
 			}
 			return null;
+		}
+
+		private static boolean isWhite(ChessPiece c){
+			switch(c){
+				case WHITE_CASTLE: case WHITE_PAWN: case WHITE_KING:
+				case WHITE_QUEEN: case WHITE_BISHOP: case WHITE_KNIGHT:{ return true; }
+				default:{return false;}
+			}
+		}
+
+		private static boolean isBlack(ChessPiece c){
+			switch(c){
+				case BLACK_CASTLE: case BLACK_PAWN: case BLACK_KING:
+				case BLACK_QUEEN: case BLACK_BISHOP: case BLACK_KNIGHT:{ return true; }
+				default:{return false;}
+			}
 		}
 	}
 
@@ -69,10 +93,21 @@ public class CG{
 	}
 
 	private static void startGame(){
-		boolean play = false;
-		while(play){}
+		boolean play = true;
+		while(play){
+			System.out.println(currentPlayer(cp));
+			printMat(board);
+			ArrayList<Properties> p = ChessPiece.possibleMoves();
+			cp++;
+			play = false;
+		}
+		//ArrayList<Properties> p = ChessPiece.possibleMoves();
+		//System.out.println(currentPlayer(cp));
+	}
 
-		ArrayList<Properties> p = ChessPiece.possibleMoves();
+	private static String currentPlayer(int c){
+		if (c%2 == 0) return "Black's Turn";
+		else return "White's Turn";
 	}
 
 	public static void main(String[] args){ CG m = new CG(); }
