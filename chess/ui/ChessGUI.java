8956dd0c1a0248f15fa3633e243b67b1ccdcc2c2
unknown <liorshahverdi@gmail.com>
2014-12-31 04:19:19 -0500

    .

--- chess/ui/ChessGUI.java
+++ chess/ui/ChessGUI.java
@@ -9,6 +9,8 @@ public class ChessGUI extends JFrame implements ActionListener{
 		private static final int HEIGHT = 850;
 		private static JPanel gui_board;
 		public ImageIcon black_tile, white_tile;
+		public JTextField tf;
+
 	}
 
 	public ChessView(){
@@ -17,5 +19,23 @@ public class ChessGUI extends JFrame implements ActionListener{
 		setSize(WIDTH,HEIGHT);
 		setVisible(true);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
+	}
+
+	private JPanel getJContentPane(){
+		JPanel temp = new JPanel();
+		temp.setBackground(Color.red);
+		temp.setLayout(new FlowLayout());
+
+		black_tile = new ImageIcon("images\\Black-Marble.jpg");
+		white_tile = new ImageIcon("images\\White-Marble.jpg");
+
+		gui_board = drawInitialGUIBoard();
+		temp.add(gui_board);
+
+		tf = new JTextField("Im right here");
+		tf.setEditable(false);
+		temp.add(tf);
+
+		return temp;
 	}
 }
